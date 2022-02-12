bc4ae556aff9a9c1c36ef2474f1bb3f7cab8217c
saiprocoder <95332916+saiprocoder@users.noreply.github.com>
2022-02-12 13:04:18 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+var a=prompt("Welcome to Team Gryffindor's project on Stephen Hawking! Type S, to see the slides. Type V, if you want to see video. Type W, to see the HTML website.");
+
+    if(a=="S") {
+        window.location.href="https://bit.ly/grfy-stephen"
+    }
+
+    if(a=="V") {
+        window.location.href="vid.mp4"
+    }
+
+    if(a=="W") {
+        window.location.href="content.html"
+    }
