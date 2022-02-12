dd546392d1c21a5550792a041ff23d3f251b50f8
saiprocoder <95332916+saiprocoder@users.noreply.github.com>
2022-02-12 13:04:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
-var a=prompt("Welcome to Team Gryffindor's project on Stephen Hawking! Type S, to see the slides. Type V, if you want to see video. Type W, to see the HTML website.");
+var a=prompt("Welcome to Team Gryffindor's project on Stephen Hawking! Type S, to see the slides. Type W, to see the HTML website.");
 
     if(a=="S") {
         window.location.href="https://bit.ly/grfy-stephen"
-    }
-
-    if(a=="V") {
-        window.location.href="vid.mp4"
     }
 
     if(a=="W") {
